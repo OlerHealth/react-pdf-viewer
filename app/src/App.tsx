c5065d0e5c0970127134ebda209d58c5bccfd757
jerry genser <jerrygenser@gmail.com>
2024-05-21 15:08:22 -0400

    wip: build

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Viewer } from '@react-pdf-viewer/core'
-import { Worker } from '@react-pdf-viewer/core';
+
+const workerUrl = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.0.379/pdf.worker.min.mjs"
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      {/* <Worker workerUrl ='https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.0.379/pdf.worker.min.mjs'>  */}
-      <Worker workerUrl ="/src/pdfjs/pdf.worker.min.mjs"> 
-      <Viewer fileUrl='https://www.clickdimensions.com/links/TestPDFfile.pdf'/>
-      </Worker>
+
     </div>
   )
 }
 
 export default App
+
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -5,9 +5,4 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'pdfjs-dist/build/pdf.worker.min.mjs': path.resolve(__dirname, 'src/pdfjs/pdf.worker.min.mjs')
-    }
-  }
 })
